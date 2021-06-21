50bf64aabc813ed10e61ae4fe03a4c52dc31040a
Juan Perez <jperez4432@gmail.com>
2021-06-21 15:17:47 -0500

    limit exercises

--- limit_exercises.sql
+++ limit_exercises.sql
@@ -6,7 +6,8 @@ WHERE birth_date LIKE '1954-01%'
 ORDER BY birth_date, last_name
 LIMIT 20 OFFSET 20;
 
+SELECT DISTINCT last_name FROM employees ORDER BY last_name DESC LIMIT 10;
 
-SELECT DISTINCT last_name FROM employees LIMIT 10;
+SELECT emp_no FROM salaries ORDER BY salary DESC LIMIT 5;
 
-SELECT emp_no FROM salaries ORDER BY salary LIMIT 5;
+SELECT emp_no FROM salaries ORDER BY salary DESC LIMIT 5 OFFSET 45;
